fee02ecb2e5d4ff46a228e12e67cb5b9e62964e8
Jacquelyn Bradshaw <jacquelyn_bradshaw@live.co.uk>
2023-01-20 20:14:25 +0000

    player config complete

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,32 @@
+let editedPlayer = 0;
+
+const players = [
+  {
+    name: "",
+    symbol: "X"
+  },
+  {
+    name: "",
+    symbol: "O"
+  },
+];
+
 const editPlayer1Button = document.querySelector(".edit-player1-button");
 const editPlayer2Button = document.querySelector(".edit-player2-button");
+
+const cancelConfigButton = document.querySelector(".cancel-config-button");
 
 const overlayAsideElement = document.querySelector(".modal");
 const backdropElement = document.querySelector(".backdrop");
 
+const formElement = document.querySelector("form");
+
+const playerValidationParagraph = document.querySelector(".config-errors");
+
 editPlayer1Button.addEventListener("click", openPlayerConfig);
 editPlayer2Button.addEventListener("click", openPlayerConfig);
+
+backdropElement.addEventListener("click", closePlayerConfig);
+cancelConfigButton.addEventListener("click", closePlayerConfig);
+
+formElement.addEventListener("submit", savePlayerConfig);
--- scripts/config.js
+++ scripts/config.js
@@ -1,4 +1,32 @@
-function openPlayerConfig() {
+function openPlayerConfig(event) {
+  editedPlayer = +event.target.dataset.playerid;
   overlayAsideElement.style.display = "block";
   backdropElement.style.display = "block";
+}
+
+function closePlayerConfig() {
+  overlayAsideElement.style.display = "none";
+  backdropElement.style.display = "none";
+  formElement.firstElementChild.classList.remove("error");
+  playerValidationParagraph.textContent = "";
+  formElement.firstElementChild.lastElementChild.value = "";
+}
+
+function savePlayerConfig(event) {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const enteredPlayerName = formData.get("player-name").trim();
+
+  if (!enteredPlayerName) {
+    event.target.firstElementChild.classList.add("error");
+    playerValidationParagraph.textContent = "Please enter a name";
+    return;
+  }
+
+  const updatedPlayerNameElement = document.querySelector(".player" + editedPlayer + "-data");
+  updatedPlayerNameElement.children[1].textContent = enteredPlayerName;
+
+  players[editedPlayer - 1].name = enteredPlayerName;
+
+  closePlayerConfig();
 }
