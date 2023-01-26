0c6f8f2b559e8721e296d3cb6392ee82d15df5d7
Jacquelyn Bradshaw <jacquelyn_bradshaw@live.co.uk>
2023-01-26 19:50:43 +0000

    checking for a winner or a draw

--- scripts/game.js
+++ scripts/game.js
@@ -27,8 +27,54 @@ function selectGameField(event) {
   event.target.classList.add("disabled");
   
   gameData[row][column] = activePlayer + 1;
-  console.log(gameData);
   
+  const winnerId = checkForGameOver();
+  console.log(winnerId);
+  
+  currentRound++;
   switchPlayer();
   playerTurnElement.textContent = players[activePlayer].name;
+}
+
+function checkForGameOver() {
+  // Checking the rows for equality
+  for (let i = 0; i <= 2; i++) {
+    if (
+      gameData[i][0] > 0 && 
+      gameData[i][0] === gameData[i][1] &&
+      gameData[i][1] === gameData[i][2]
+      ) {
+        return gameData[i][0];
+      }
+  }
+  // Checking the columns for equality
+  for (let i = 0; i <= 2; i++) {
+    if (
+      gameData[0][i] > 0 && 
+      gameData[0][i] === gameData[1][i] &&
+      gameData[1][i] === gameData[2][i]
+      ) {
+        return gameData[0][i];
+      }
+  }
+  // Checking the diagonals for equality
+  if (
+    gameData[0][0] > 0 && 
+    gameData[0][0] === gameData[1][1] &&
+    gameData[1][1] === gameData[2][2]
+    ) {
+      return gameData[0][0];
+    }
+
+  if (
+    gameData[0][2] > 0 && 
+    gameData[0][2] === gameData[1][1] &&
+    gameData[1][1] === gameData[2][0]
+    ) {
+      return gameData[0][2];
+    }
+    if (currentRound === 9) {
+      return -1;
+    }
+  return 0;
 }
